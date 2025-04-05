95d5b6f986bcc1376ea3325d6d43985e324d5557
puja349 <pg6139901@gmail.com>
2025-04-05 19:06:22 +0000

    updated the code and made it able to recognize the blank input

--- fundamentals.c
+++ fundamentals.c
@@ -6,24 +6,26 @@
 void fundamentals(void) {
     /* Version 2: Measuring Strings */
     printf("*** Start of Measuring Strings Demo ***\n");
-    char buffer2[BUFFER_SIZE];
+    
+    char str[BUFFER_SIZE];
+
     do {
         // Ask user to input a string
-        printf("Type a string (q to quit):\n");
-        fgets(buffer2, BUFFER_SIZE, stdin);
+        printf("Type a string (q - to quit):\n");
+        fgets(str, BUFFER_SIZE, stdin);
+        str[strcspn(str, "\n")] = '\0'; // remove newline
 
-        if (strcmp(buffer2, "q\n") != 0) {
-            buffer2[strlen(buffer2) - 1] = '\0'; // Remove newline
-
-             if (strcmp(str, "") == 0 || strspn(str, " ") == strlen(str)) {
-                printf("Warning: blank input\n");
-            } else if (strcmp(str, "q") != 0) {
-            // Show the length of the input string
-            printf("The length of \"%s\" is %d characters\n", buffer2, (int)strlen(buffer2));
+        if (strcmp(str, "") == 0 || strspn(str, " ") == strlen(str)) {
+            printf("Warning: blank input\n");
+        } else if (strcmp(str, "q") != 0) {
+            printf("The length of \"%s\" is %lu characters\n", str, strlen(str));
         }
-    } while (strcmp(buffer2, "q\n") != 0);
-    printf("*** End of Measuring Strings Demo ***\n\n");
+
+    } while (strcmp(str, "q") != 0);
+
+    printf("--- End of Measuring Strings ---\n\n");
 }
+
 
 
 
